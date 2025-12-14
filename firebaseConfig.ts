873f018009bf7af4a9e2e0fc062388df915fa446
Guaroduino <Guaroduino@gmail.com>
2025-12-14 13:06:42 -0400

    fixes

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,7 +1,7 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-storage.js";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // IMPORTANT: Replace the placeholder values below with your app's Firebase project configuration.
 // You can find this configuration in your Firebase project settings.
